67ce0ce4f6fd19e552cb54b53d59963667bf0079
yoyoo <mcwatney@naver.com>
2023-10-28 06:48:27 +0900

    FEAT : add diray controller

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { DiaryService } from './diary.service';
+
+// AS-IS
+// POST /question
+// GET /answer/:id
+// GET /answerer/:questionId
+// POST /answer/:questionId
+// GET /cookie
+
+// TO-BE
+// POST /diary/question
+// GET /diary/answer/:id
+// GET /diary/answerer/:questionId
+// POST /diary/answer/:questionId
+// GET /cookie
 
 @Controller('diary')
-export class DiaryController {}
+export class DiaryController {
+  constructor(private readonly diaryService: DiaryService) {}
+  @Post('question')
+  async postQuestion(@Body() body: any) {
+    return this.diaryService.postQuestion(body);
+  }
+  @Get('answer/:questionId')
+  async getAnswer(@Param('questionId') questionId: string) {
+    return this.diaryService.getAnswer();
+  }
+
+  @Get('answerers/:questionId')
+  async getAnswerers(@Param('questionId') questionId: string) {
+    return this.diaryService.getAnswerers(questionId);
+  }
+
+  @Post('answer/:questionId')
+  async postAnswer(@Param('questionId') questionId: string, @Body() body: any) {
+    return this.diaryService.postAnswer({ questionId, answer: body });
+  }
+}
