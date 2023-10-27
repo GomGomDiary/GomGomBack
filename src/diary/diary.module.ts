bbc7dd6e85de5076c4b9369bc4ad127d410f1084
yoyoo <mcwatney@naver.com>
2023-10-28 06:47:46 +0900

    FEAT : add diary module

--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DiarySchema } from './diary.schema';
+import { Diary } from './diary.schema';
+import { DiaryRepository } from './diary.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Diary.name, schema: DiarySchema }]),
+  ],
   controllers: [DiaryController],
-  providers: [DiaryService]
+  providers: [DiaryService, DiaryRepository],
 })
 export class DiaryModule {}
