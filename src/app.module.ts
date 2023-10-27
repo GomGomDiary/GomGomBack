a8930a462c73d11a3d518d52512768ecc882a0dc
yoyoo <mcwatney@naver.com>
2023-10-28 06:47:05 +0900

    FEAT : update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DiaryModule } from './diary/diary.module';
 
+import config from './config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [ConfigModule.forRoot(), DiaryModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      envFilePath: `./.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    DiaryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
