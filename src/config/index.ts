ca8d3cb56910ae02229e4eaafaed9d9db8883add
yoyoo <mcwatney@naver.com>
2023-10-28 06:43:52 +0900

    FEAT : add configuration file

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+
+export default () => ({
+  PORT: process.env.PORT ?? 8765,
+  MONGO_URI: process.env.MONGO_URI as string,
+  COOKIE_SECRET: process.env.COOKIE_SECRET ?? '1234',
+  COOKIE_OPTION: {
+    httpOnly: true,
+    signed: NODE_ENV === 'production' ? true : false,
+    expires: new Date(253402300000000),
+  },
+});
