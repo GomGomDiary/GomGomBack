46673a33f50bb719c9a6d645f5458466b0e132ec
yoyoo <mcwatney@naver.com>
2023-10-28 06:47:22 +0900

    FEAT : add diary schema

---
+++ src/diary/diary.schema.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { SchemaOptions, Document } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Answer extends Document {
+  @Prop()
+  answerer: string;
+  @Prop()
+  answers: any[];
+}
+
+const AnswerSchema = SchemaFactory.createForClass(Answer);
+
+@Schema(options)
+export class Diary extends Document {
+  @Prop({
+    required: true,
+  })
+  questioner: string;
+
+  @Prop()
+  question: any[];
+
+  @Prop()
+  challenge: string;
+
+  @Prop()
+  countersign: string;
+
+  @Prop({
+    type: [AnswerSchema],
+    // _id: true,
+  })
+  answerList: Answer[];
+}
+
+export const DiarySchema = SchemaFactory.createForClass(Diary);
