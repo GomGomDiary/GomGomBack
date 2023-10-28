3bdd4dbc4759bd0b076d6e3a108b201738c37027
yoyoo <mcwatney@naver.com>
2023-10-28 09:05:09 +0900

    FEAT : register cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
+  const cookieSecret = configService.get<string>('COOKIE_SECRET');
+  app.use(cookieParser(cookieSecret));
   await app.listen(port);
 }
 bootstrap();
