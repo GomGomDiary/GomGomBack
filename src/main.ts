2f5878e3c979a4953c5c820827d0b873923fc6d6
yoyoo <mcwatney@naver.com>
2023-11-01 06:56:57 +0900

    REFACTOR : logging PORT

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   const cookieSecret = configService.get<string>('COOKIE_SECRET');
+  const logger = new Logger();
+
+  logger.log(`PORT: ${port}`);
   app.use(cookieParser(cookieSecret));
   app.useGlobalPipes(new ValidationPipe());
+
   // TODO
   // app.enableShutdownHooks();
   await app.listen(port);
