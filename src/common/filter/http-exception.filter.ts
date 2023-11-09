1300288453807b3627cd3e043b3a8c17319ca1a2
yoyoo <mcwatney@naver.com>
2023-11-09 17:24:00 +0900

    REFACTOR : update filter

--- src/common/filter/http-exception.filter.ts
+++ src/common/filter/http-exception.filter.ts
@@ -22,13 +22,24 @@ export class HttpExceptionFilter implements ExceptionFilter {
     }
 
     logger.error(`[${request.method}] ${request.url} ${user}`);
-
     const now = new Date();
     const koreaTimeDiff = 9 * 60 * 60 * 1000;
+    const error = exception.getResponse();
 
+    if (typeof error === 'string') {
+      return response.status(status).json({
+        statusCode: status,
+        error,
+        timestampUtc: now.toISOString().replace('Z', ''),
+        timestampKst: new Date(now.getTime() + koreaTimeDiff)
+          .toISOString()
+          .replace('Z', ''),
+        path: request.url,
+      });
+    }
     response.status(status).json({
       statusCode: status,
-      message: exception.message,
+      ...error,
       timestampUtc: now.toISOString().replace('Z', ''),
       timestampKst: new Date(now.getTime() + koreaTimeDiff)
         .toISOString()
