4a0237d981065bb58fadce257ae73a48b3e879f9
yoyoo <mcwatney@naver.com>
2023-11-09 17:27:04 +0900

    FEAT : add auth module in diary module

--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,16 +7,21 @@ import { Diary } from './diary.schema';
 import { DiaryRepository } from './diary.repository';
 import { ConfigModule } from '@nestjs/config';
 import config from 'src/config';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule,
     ConfigModule.forRoot({
       load: [config],
       envFilePath: `./.env.${process.env.NODE_ENV}`,
     }),
     MongooseModule.forFeature([{ name: Diary.name, schema: DiarySchema }]),
+    forwardRef(() => AuthModule),
   ],
   controllers: [DiaryController],
   providers: [DiaryService, DiaryRepository],
+  exports: [DiaryRepository],
 })
 export class DiaryModule {}
