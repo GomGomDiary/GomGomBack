937531c69468a425782e0759477ea3f1ddc2d340
yoyoo <mcwatney@naver.com>
2023-12-07 13:55:35 +0900

    TEST : Adding tests to auth

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { DiaryRepository } from 'src/diary/repository/diary.repository';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  const mockDiaryRepository = { findField: jest.fn() };
+  const mockJwtService = { signAsync: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: DiaryRepository,
+          useValue: mockDiaryRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should return a jwt token when sign-in is successful', async () => {
+      const mockUser = { countersign: '1234', _id: '12345678' };
+
+      mockDiaryRepository.findField.mockResolvedValue(mockUser);
+      mockJwtService.signAsync.mockResolvedValue('jwtToken');
+
+      const result = await authService.signIn('someId', '1234');
+      expect(result).toEqual({ diaryToken: 'jwtToken' });
+    });
+
+    it('should throw UnauthorizedException when countersign is incorrect', async () => {
+      const mockUser = { countersign: '1234', _id: '12345678' };
+
+      mockDiaryRepository.findField.mockResolvedValue(mockUser);
+
+      const signIn = authService.signIn('someId', 'wrongCountersign');
+      await expect(signIn).rejects.toBeInstanceOf(UnauthorizedException);
+    });
   });
 });
