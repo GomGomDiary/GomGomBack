c064290ff0ae6386b4ca37d22a0eec5feff81512
yoyoo <mcwatney@naver.com>
2023-12-10 01:42:41 +0900

    FIX : A bug fix
    Related to : #69

--- src/common/decorators/mongoIdTransform.decorator.ts
+++ src/common/decorators/mongoIdTransform.decorator.ts
@@ -5,8 +5,9 @@ import mongoose from 'mongoose';
 export const MongoIdTransfrom =
   (options?: ExposeOptions) => (target, propertyKey) => {
     Transform(({ value }) => {
-      if (!!value && !mongoose.isValidObjectId(value))
+      if ((!!value && !mongoose.isValidObjectId(value)) || value === '') {
         throw new BadRequestException(`${propertyKey}가 형식에 맞지 않습니다.`);
+      }
       return new mongoose.Types.ObjectId(value);
     }, options)(target, propertyKey);
   };
